4c97aabfe0ded4f7b7ae28d0ed76f53c370b50b0
KIN <67863453+KIN-002@users.noreply.github.com>
2024-10-10 20:08:50 +0800

    刪減數電內容 (#125)
    * 刪減數電內容
    
    應課程組老師要求刪減
    
    * Delete uart.(28).cnf.hdb

--- 专业课/数字系统基础(電信)/實驗代碼參考/5.4 uart_wanzhengban/10 ADshi qian/PLL2_inst.v
+++
@@ -1,4 +0,0 @@
-PLL2	PLL2_inst (
-	.inclk0 ( inclk0_sig ),
-	.c0 ( c0_sig )
-	);
--- 专业课/数字系统基础(電信)/實驗代碼參考/5.4 uart_wanzhengban/source/rx_fifo_module_inst.v
+++
@@ -1,9 +0,0 @@
-rx_fifo_module	rx_fifo_module_inst (
-	.clock ( clock_sig ),
-	.data ( data_sig ),
-	.rdreq ( rdreq_sig ),
-	.wrreq ( wrreq_sig ),
-	.empty ( empty_sig ),
-	.full ( full_sig ),
-	.q ( q_sig )
-	);
